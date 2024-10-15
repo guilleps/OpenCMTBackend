263d3133986c08f553f1685e090aac391723fc3f
Guillermo Palacios <nothicc04@gmail.com>
2024-10-15 02:33:46 -0500

    setup-project

---
+++ src/main/java/com/open/cmt/OpenCmtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.open.cmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenCmtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenCmtBackendApplication.class, args);
+	}
+
+}
