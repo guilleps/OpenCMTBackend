bd90489e27180b790ac3e41005b7650dbc204e1b
Raul Abanto <raul0207abanto@gmail.com>
2024-11-09 23:28:19 -0500

    fix/zona-horaria

--- src/main/java/com/open/cmt/OpenCmtBackendApplication.java
+++ src/main/java/com/open/cmt/OpenCmtBackendApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableSpringDataWebSupport
 public class OpenCmtBackendApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Bogota"));
 		SpringApplication.run(OpenCmtBackendApplication.class, args);
+
 	}
 
 }
